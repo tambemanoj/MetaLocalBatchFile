50f7f7e3666da1b233c51ac7ac388e61410278ee
tambemanoj <92679495+tambemanoj@users.noreply.github.com>
2024-06-17 19:17:43 +0530

    Delete Update server.js

--- Update server.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const app = express();
-
-app.use(express.static('public'));
-
-const PORT = process.env.PORT |8080;
-
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
